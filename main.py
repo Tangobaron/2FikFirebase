13b26dc8bb530d574ea98c25dc71e5360deef93a
nicolas-pare <nicolas@laboiteinteractive.com>
2021-04-22 12:47:47 -0400

    add td_client class and change the main.py structure

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import firebase_admin
 
 from firebase_admin import credentials, firestore
+from td_client import TDClient
 import time
+import socket
+import sys
 
 cred = credentials.Certificate("securityAccountKey.json")
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
+#server related variable
+TD_CLI = TDClient('localhost', 5786) 
+isAlive = False
+def main():
+    global isAlive
+    if isAlive is False:
+        # Creates a reference to the messages collection
+        doc_ref = db.collection('messages').order_by('time', direction=firestore.Query.DESCENDING).limit(1)
+        doc_watch = doc_ref.on_snapshot(on_snapshot)
+        isAlive = True
 
 def profile_names(uid):
     names_ref = db.collection(u'profiles').get()
@@ -17,9 +29,9 @@ def profile_names(uid):
             return names
 
 
+
 def on_snapshot(doc_snapshot, changes, read_time):
-    print(
-        '_____________________________________________________________________________________________________________')
+    print('_____________________________________________________________________________________________________________')
 
     for doc in doc_snapshot:
         messages = doc.to_dict()
@@ -27,19 +39,27 @@ def on_snapshot(doc_snapshot, changes, read_time):
         recipient = profile_names(messages.get('to'))
         time_of_reception = messages.get('time')
         text = messages.get('body')
-
-        print(f"sender:     {sender}")
-        print(f"recipient:  {recipient}")
-        print(f"time:       {time_of_reception}")
-        print(f"text:       {text}")
-        print(
-            '_____________________________________________________________________________________________________________')
-
-
-# Creates a reference to the messages collection
-doc_ref = db.collection('messages').order_by('time', direction=firestore.Query.DESCENDING).limit(10)
-doc_watch = doc_ref.on_snapshot(on_snapshot)
+        nameList = ["sender", "recipient", "time", "text"]
+        dataList = [str(sender), str(recipient), str(time_of_reception), str(text)]
+        TD_CLI.SendMessage(nameList, dataList)
+        #print(f"sender:     {sender}")
+        #print(f"recipient:  {recipient}")
+        #print(f"time:       {time_of_reception}")
+        #print(f"text:       {text}")
+        print('_____________________________________________________________________________________________________________')
 
 # Keep the app running
+try:
+	main()
+except err:
+	print(err)
+
 while True:
     time.sleep(0.1)
+
+#if __name__=="__main__":
+#    try:
+#        main()
+#    except KeyboardInterrupt:
+#        print('interrupted')
+#        pass
