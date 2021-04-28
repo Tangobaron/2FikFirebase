89bc6fd78c16732ee5aaec971fed03eede9eaf06
nicolas-pare <nicolas@laboiteinteractive.com>
2021-04-28 14:15:31 -0400

    finish the function that query email and location of 2fik. The data is correctly transfer to the touch designer server at this point

--- main.py
+++ main.py
@@ -6,61 +6,77 @@ from twofik_localisation import Twofik
 import time
 import socket
 import sys
+import threading
 
 cred = credentials.Certificate("venv/securityAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+sVar = None
 
 #server related variable
 class serverVar():
+    testing = False
     TD_CLI = TDClient('localhost', 5786)
     isAlive = False
-    initMessage = False
     twoFik = Twofik(cred,db,'uTS21weWNkbggwHu16ScM1Nqart1')
     doc_watch = None
+    terminateThread = False
+    lastName = None
+    initMessage = False
+    callbackDone = threading.Event()
 
 
 def main():
-    if serverVar.isAlive is False:
+    global sVar
+    if sVar is None:
+        sVar = serverVar()
+    if sVar.isAlive is False:
+        if sVar.testing: print("Initialise server")
         # Creates a reference to the messages collection
         #doc_ref = db.collection('messages').order_by('time', direction=firestore.Query.DESCENDING).limit(5)
-        serverVar.isAlive = init()
+        sVar.isAlive = init()
+    if sVar.lastName != sVar.twoFik.ChatWith:
+        if sVar.testing: print("----------------------| updating message query")
+        queryChat()
+        sVar.lastName = sVar.twoFik.ChatWith
 
 # Follow 2fik location in real time
 
 def init():
-
+    global sVar
     #twoFikID = twofik_location(True)
     # start listener on message collection
     #collection_ref = db.collection('messages')
     #fik_ref = collection_ref.where(u'from', u'==',  twoFikID).limit(10) 
-    queryChat()
     #serverVar.doc_watch = fik_ref.on_snapshot(on_snapshot)
     # create twoFik status object and start listening on it<s in app location
-    serverVar.twoFik.Follow2fik()
+    sVar.twoFik.Follow2fik()
     #watch(twoFik.Name,callback=ChangeQuery)
-    watch(serverVar.twoFik.ChatWith,callback=ChangeQuery)
+    #watch(sVar.twoFik.Name,callback=ChangeQuery)
     return True
 
-    def iniInbox():
-        Inbox.init(twoKif.Name)
-
 def queryChat():
-    print('Entering queryChat')
+    global sVar
     collection_ref = db.collection('messages')
-    fik_ref = collection_ref.where(u'from', u'==',  serverVar.twoFik.Name).limit(30)
-    profile_ref = fik_ref.where(u'to', u'==', serverVar.twoFik.ChatWith)
-    if serverVar.doc_watch:
-        print("remove listener")
-        serverVar.doc_watch.remove()
+    if sVar.testing: print(f'twofik id: {sVar.twoFik.Name}')
+    fik_ref = collection_ref.where(u'from', u'==',  sVar.twoFik.Name).limit(30)
+    if sVar.testing: print(f'Twofik ID: {sVar.twoFik.Name}')
+    profile_ref = fik_ref.where(u'to', u'==', sVar.twoFik.ChatWith)
+    sVar.doc_watch = profile_ref.on_snapshot(on_snapshot)
+    if sVar.doc_watch is not None:
+        if sVar.testing: print("remove listener after the callback trigger")
+        sVar.terminateThread = True
+        sVar.callbackDone.wait(timeout=30)
+        sVar.doc_watch.unsubscribe()
+        sVar.initMessage = False
     try:
-        serverVar.doc_watch = profile_ref.on_snapshot(on_snapshot)
+        sVar.doc_watch = profile_ref.on_snapshot(on_snapshot)
     except:
-        print('cannot create snapshot object')
-    print('finished with queryChat')
+        if sVar.testing: print('cannot create snapshot object')
+    if sVar.testing: print('finished with queryChat')
 
 def ChangeQuery(frame, elem, exec_info):
-    print("change query but fonction is not finish")
+    if sVar.testing: print("change query but fonction is not finish")
     queryChat()
 
 # Extract the names of the 2Fik profiles according to their UIDs
@@ -70,8 +86,7 @@ def twofik_profile_names():
         if i.get('state') == '2fik':
             names = i.get('name')
             uid = i.id
-            print(f'{uid} ->  {names}')
-
+            if sVar.testing: print(f'{uid} ->  {names}')
 
 # Gets a single real name from a UID
 def get_real_name(uid):
@@ -79,7 +94,6 @@ def get_real_name(uid):
     for name in names_ref:
         if uid == name.id:
             return name.get('name')
-
 
 # Where is 2Fik in the app and which profile is he using
 def twofik_location(getID = False):
@@ -101,11 +115,11 @@ def twofik_location(getID = False):
     # Which panel is 2Fik on
     panel = dictionary['panel']
     panel_state = panel.get('state')
-    print('______________________________________________________________________________')
-    print(f'profile used : {profile_selected}')
-    print(body_state)
-    print(panel_state)
-    print('______________________________________________________________________________')
+    if sVar.testing: print('______________________________________________________________________________')
+    if sVar.testing: print(f'profile used : {profile_selected}')
+    if sVar.testing: print(body_state)
+    if sVar.testing: print(panel_state)
+    if sVar.testing: print('______________________________________________________________________________')
     if getID == True:
         return profile_selected
     else:
@@ -114,56 +128,60 @@ def twofik_location(getID = False):
 
 # Receives each messages depending on which persona is 2Fik incarning
 def on_snapshot(doc_snapshot, changes, read_time):
-    #print('_____________________________________________________________________________________________________________')
-    
-    if serverVar.initMessage is True:
+    if sVar.testing: print('_____________________________________________________________________________________________________________')
+    if sVar.initMessage is True:
         for change in changes:
-            print(f'changeType: {change.type.name}')
+            if sVar.testing: print(f'changeType: {change.type.name}')
             if change.type.name == 'ADDED':
-                #doc = change.document.id
-                doc = change.document.to_dict()
-                print('_____________________________________________________________________________________________________________')
-                print(f'doc: {doc}')
-                print('_____________________________________________________________________________________________________________')
+                doc = change.document
+                #doc = change.after()
+                messages = doc.to_dict()
+                if sVar.testing: print('__________________________________________________\\\\_______________________________________________________')
+                if sVar.testing: print(f'messages: {messages}')
+                if sVar.testing: print('__________________________________________________\\\\\______________________________________________________')
                 sender = get_real_name(messages.get('from'))
-                recipient = get_real_name(messages.get('to'))
+                recipientID = messages.get('to')
+                recipient = get_real_name(recipientID)
                 #print(f'recipient: {recipient}')
                 time_of_reception = messages.get('time')
                 text = messages.get('body')
-                nameList = ["sender", "recipient", "time", "text"]
-                dataList = [str(sender), str(recipient), str(time_of_reception), str(text)]
-                TD_CLI.SendMessage(nameList, dataList)
-
+                nameList = ["sender", "recipient", "recipientID", "time", "text"]
+                dataList = [str(sender), str(recipient), str(recipientID), str(time_of_reception), str(text)]
+                sVar.TD_CLI.AddToBuffer(nameList, dataList)
+        sVar.TD_CLI.SendMessage()
     else:
+        if sVar.testing: print('entering else')
+        if sVar.testing: print(f'doc_snapshot: {doc_snapshot}' )
         for doc in doc_snapshot:
             messages = doc.to_dict()
             #print(f'messages: {messages}')
             sender = get_real_name(messages.get('from'))
-            recipient = get_real_name(messages.get('to'))
+            recipientID = messages.get('to')
+            recipient = get_real_name(recipientID)
             #print(f'recipient: {recipient}')
             time_of_reception = messages.get('time')
             text = messages.get('body')
-            nameList = ["sender", "recipient", "time", "text"]
-            dataList = [str(sender), str(recipient), str(time_of_reception), str(text)]
-            serverVar.TD_CLI.SendMessage(nameList, dataList)
-            #print(f"sender:     {sender}")
-            #print(f"recipient:  {recipient}")
-            #print(f"time:       {time_of_reception}")
-            #print(f"text:       {text}")
-            #print('_____________________________________________________________________________________________________________')
-        initMessage = True
-# Keep the app running
-try:
-	main()
-except KeyboardInterrupt:
-        print('interrupted')
-        pass
-#except Exception as e:
-	#print(f'an error occured in main function: {repr(e)}')
+            nameList = ["sender", "recipient", "recipientID", "time", "text"]
+            dataList = [str(sender), str(recipient), str(recipientID), str(time_of_reception), str(text)]
+            sVar.TD_CLI.AddToBuffer(nameList, dataList)
+            if sVar.testing: print(f"sender:     {sender}")
+            if sVar.testing: print(f"recipient:  {recipient}")
+            if sVar.testing: print(f"time:       {time_of_reception}")
+            if sVar.testing: print(f"text:       {text}")
+            if sVar.testing: print('_____________________________________________________________________________________________________________')
+        sVar.initMessage = True
+        sVar.TD_CLI.SendMessage()
+    sVar.callbackDone.set()
+    if sVar.testing: print("finished query")
+
 
 while True:
-    time.sleep(0.1)
-
+    try:
+        time.sleep(0.1)
+        main()
+    except KeyboardInterrupt:
+        if sVar.testing: print('manual interupt')
+        sys.exit()
 #if __name__=="__main__":
 #    try:
 #        main()
--- twofik_localisation.py
+++ twofik_localisation.py
@@ -6,7 +6,8 @@ from firebase_admin import credentials, firestore
 from td_client import TDClient
 
 class Twofik:
-    def __init__(self, cred, db, identification):
+    def __init__(self, cred, db, identification, DEBUGMODE = False):
+        self.DEBUG = False
         self.credential = cred
         self.db = db
         self.cli = TDClient('localhost', 5784)
@@ -14,39 +15,40 @@ class Twofik:
         self.lastSpeakWith = None
         #variable related to 2fik location
         self.Name = None
+        self.personaID = None
         self.VisitedProfile = None
         self.ChatWith = None
         self.BodyLocation = None
         self.PanelLocation = None
 
     def on_snapshot(self, doc_snapshot, changes, read_time):
-        print("enter on snapshot")
+        if self.DEBUG: print("enter on snapshot")
         for doc in doc_snapshot:
             status = doc.to_dict()
             body = status.get('body')
             panel = status.get('panel')
-            print(f'status: {status}')
+            if self.DEBUG: print(f'status: {status}')
             #-------------name---------------
             name = status.get('profile')
             if name != self.Name:
                 self.ChatWith = None
             self.Name = name
-            print(f'Name: {self.Name}')
+            if self.DEBUG: print(f'Name: {self.Name}')
             #-----------location-------------
             self.BodyLocation = body.get('state')
             self.PanelLocation = panel.get('state')
-            print(f'BodyLocation: {self.BodyLocation}')
-            print(f'PanelLocation: {self.PanelLocation}')
+            if self.DEBUG: print(f'BodyLocation: {self.BodyLocation}')
+            if self.DEBUG: print(f'PanelLocation: {self.PanelLocation}')
             #---------visit profile----------
             body_profile = body.get('profile')
             if body_profile is not None:
                 self.VisitedProfile = body_profile
-            print(f'body_profile: {self.VisitedProfile}')
+            if self.DEBUG: print(f'body_profile: {self.VisitedProfile}')
             #-----------chat with-------------
             if panel.get('state') == "chat":
                 profile = panel.get('profile')
                 self.ChatWith = profile
-            print(f'ChatWith: {self.ChatWith}')
+            if self.DEBUG: print(f'ChatWith: {self.ChatWith}')
         self.sendUpdate()
     
     def get_real_name(self, uid):
@@ -56,12 +58,13 @@ class Twofik:
                 return name.get('name')
 
     def sendUpdate(self):
-        nameList = ["Name", "Body Location", "Panel Location", "Visited Profile", "Chat With"]
-        dataList = [str(self.get_real_name(self.Name)), str(self.BodyLocation), str(self.PanelLocation), str(self.VisitedProfile), str(self.get_real_name(self.ChatWith))]
-        self.cli.SendMessage(nameList, dataList)
+        nameList = ["Chat_ID", "Name", "Body_Location", "Panel_Location", "Visited_Profile", "Chat_With"]
+        dataList = [self.ChatWith, str(self.get_real_name(self.Name)), str(self.BodyLocation), str(self.PanelLocation), str(self.VisitedProfile), str(self.get_real_name(self.ChatWith))]
+        self.cli.AddToBuffer(nameList, dataList)
+        self.cli.SendMessage()
 
     def Follow2fik(self):
-        print(f'Following twofik at id: {self.twofikID}')
+        if self.DEBUG: print(f'Following twofik at id: {self.twofikID}')
         collection_ref = self.db.collection("location").document(self.twofikID)
         locationUpdate = collection_ref.on_snapshot(self.on_snapshot)
 
@@ -82,11 +85,11 @@ class Twofik:
         # Which panel is 2Fik on
         panel = dictionary['panel']
         panel_state = panel.get('state')
-        print('______________________________________________________________________________')
-        print(f'profile used : {profile_selected}')
-        print(body_state)
-        print(panel_state)
-        print('______________________________________________________________________________')
+        if self.DEBUG: print('______________________________________________________________________________')
+        if self.DEBUG: print(f'profile used : {profile_selected}')
+        if self.DEBUG: print(body_state)
+        if self.DEBUG: print(panel_state)
+        if self.DEBUG: print('______________________________________________________________________________')
         if getID == True:
             return profile_selected
         else:
