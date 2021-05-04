b3c527fa817d33de1ebd727068cd7357d7be5461
Tangobaron <raphaeltangobaron@gmail.com>
2021-05-04 13:32:13 -0400

    Class now returns two lists filled with people depending on the amount of hot or not votes they have. Missing logic for the profiles with the same amount of votes

--- ranking.py
+++ ranking.py
@@ -2,26 +2,32 @@ from firebase_admin import firestore
 
 from td_client import TDClient
 
-db = firestore.client()
-
 
 class Ranking:
 
-    def __init__(self):
+    def __init__(self, database):
+        self.db = database
         self.not_leaderboard = []
         self.hot_leaderboard = []
-        self.cli = TDClient('localhost', 1)
+        # self.cli = TDClient('localhost', 1)
 
         # Reference to all of 2Fik profiles
-        rank_ref = db.collection('profiles').where(u'state', u'==', '2fik').stream()
+        rank_ref = self.db.collection('profiles').where(u'state', u'==', '2fik').stream()
 
         # Extracts all the
+        # TODO add logic to make sure people with the same amount of votes are organized depending on the amount of opposite vote and alphabetical order
+
         for rank in rank_ref:
             name = rank.get('name')
             hot_count = rank.get('hotCount')
             not_count = rank.get('notCount')
 
             if hot_count > not_count:
-                self.hot_leaderboard.append(name, hot_count)
+                hot_dict = {name, hot_count}
+                self.hot_leaderboard.append(hot_dict)
             elif not_count > hot_count:
-                self.not_leaderboard.append(name, not_count)
+                not_dict = {name, not_count}
+                self.not_leaderboard.append(not_dict)
+
+        print(f'hot leaderboard -> {self.hot_leaderboard}')
+        print(f'not leaderboard -> {self.not_leaderboard}')
