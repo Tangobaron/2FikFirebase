62937f114c914514afa07711a6e89404ab24b3ec
Tangobaron <raphaeltangobaron@gmail.com>
2021-05-04 12:05:40 -0400

    Added list with timestamp and last message to dictionary with names as keys

--- inbox.py
+++ inbox.py
@@ -2,12 +2,11 @@ from firebase_admin import firestore
 
 from td_client import TDClient
 
-db = firestore.client()
-
 
 class Inbox:
-    def __init__(self):
-        self.cli = TDClient('localhost', 1)
+    def __init__(self, database):
+        self.db = database
+        # self.cli = TDClient('localhost', 1)
         self.persons = {}
         self.maxIndex = 20
 
@@ -15,41 +14,33 @@ class Inbox:
     def CalculateInbox(self, identification):
 
         # Fetch all the latest messages addressed to the 2fik profile is currently using
-        # TODO google.api_core.exceptions.FailedPrecondition: 400 The query requires an index.
-        recipient_ref = db.collection('messages').where(u'to', u'==', identification).order_by(
+        recipient_ref = self.db.collection('messages').where(u'to', u'==', identification).order_by(
             u'time', direction=firestore.Query.DESCENDING).limit(self.maxIndex).stream()
 
         for msg in recipient_ref:
-            user = msg.get('to')
+            user = msg.get('from')
             lastMsg = msg.get('body')
-            if user in self.persons:
-                updateObj(user, lastMsg)
-            elif user not in self.persons:
-                addObject(user, lastMsg)
-            else:
-                print("sender error")
+            timestamp = msg.get('time')
+            self.addObject(user, lastMsg, timestamp)
 
-        # Fetch all the latest messages sent by the profile 2Fik is currently using
-        # TODO google.api_core.exceptions.FailedPrecondition: 400 The query requires an index.
-        sender_ref = db.collection('messages').where(u'from', u'==', identification).order_by(
-            u'time', direction=firestore.Query.DESCENDING).limit(self.maxIndex).stream()
+        for person in self.persons:
+            print(f'{person} -> {self.persons[person][0]} -> {self.persons[person][1]}')
 
-        for msg in sender_ref:
-            user = msg.get('to')
-            lastMsg = msg.get('body')
-            if user in self.persons:
-                updateObj(user, lastMsg)
-            elif user not in self.persons:
-                addObject(user, lastMsg)
-            else:
-                print("sender error")
+    # def SentMessages(self, identification):
+    #     # Fetch all the latest messages sent by the profile 2Fik is currently using
+    #     sender_ref = self.db.collection('messages').where(u'from', u'==', identification).order_by(
+    #         u'time', direction=firestore.Query.DESCENDING).limit(self.maxIndex).stream()
+    #
+    #     for msg in sender_ref:
+    #         user = msg.get('to')
+    #         lastMsg = msg.get('body')
+    #         timestamp = msg.get('time')
+    #         self.addObject(user, lastMsg, timestamp)
+    #
+    #     for person in self.persons:
+    #         print(f'{person} -> {self.persons[person][0]} -> {self.persons[person][1]}')
 
-
-# add passed user and their latest messages
-def addObject(self, user, lastMsg):
-    self.persons.add(user, lastMsg)
-
-
-# updates the passed user's latest message
-def updateObj(self, user, lastMsg):
-    self.persons[user] = lastMsg
+    # add passed user and their latest messages
+    def addObject(self, user, lastMsg, timestamp):
+        obj = [lastMsg, timestamp]
+        self.persons[user] = obj
