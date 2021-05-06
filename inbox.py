fb0d0a041fdc128705e2bf7883105fa852065efc
nicolas-pare <nicolas@laboiteinteractive.com>
2021-05-06 11:08:42 -0400

    integrate inbox function and optimize communication process between python and touch

--- inbox.py
+++ inbox.py
@@ -1,31 +1,54 @@
 from firebase_admin import firestore
-
 from td_client import TDClient
 
 
 class Inbox:
-    def __init__(self, database):
+    def __init__(self, database, cli, DEBUG=False):
         self.db = database
-        # self.cli = TDClient('localhost', 1)
+        self.testing = DEBUG
+        self.server = cli
         self.persons = {}
         self.maxIndex = 20
 
     # fills inbox with latest messages
     def CalculateInbox(self, identification):
-
         # Fetch all the latest messages addressed to the 2fik profile is currently using
-        recipient_ref = self.db.collection('messages').where(u'to', u'==', identification).order_by(
-            u'time', direction=firestore.Query.DESCENDING).limit(self.maxIndex).stream()
+        if identification is not None:
+            if self.testing is True: print('clearing self.persons')
+            self.persons = {}
+            recipient_ref = self.db.collection('messages').where(u'to', u'==', identification).order_by(
+                u'time', direction=firestore.Query.DESCENDING).limit(self.maxIndex).stream()
+            sent_ref = self.db.collection('messages').where(u'from', u'==', identification).order_by(
+                u'time', direction=firestore.Query.DESCENDING).limit(self.maxIndex).stream()
 
-        for msg in recipient_ref:
-            user = msg.get('from')
-            lastMsg = msg.get('body')
-            timestamp = msg.get('time')
-            self.addObject(user, lastMsg, timestamp)
+            for msg in recipient_ref:
+                user = msg.get('from')
+                lastMsg = msg.get('body')
+                timestamp = msg.get('time')
+                if self.testing is True: print(f'user: {user}, lastMsg: {lastMsg}, timestamp: {timestamp}')
+                self.addObject(self.persons, user, lastMsg, timestamp)
+        
+            for msg in sent_ref:
+                user = msg.get('to')
+                lastMsg = msg.get('body')
+                timestamp = msg.get('time')
+                if self.testing is True: print(f'user: {user}, lastMsg: {lastMsg}, timestamp: {timestamp}')
+                #self.addObject(persons, user, lastMsg, timestamp)
+                if user in self.persons:
+                    if timestamp > self.persons[user][1]:
+                        self.addObject(self.persons,user, lastMsg, timestamp)
+                else:
+                    self.addObject(self.persons,user, lastMsg, timestamp)
+            self.sendToServer(identification)
 
-        for person in self.persons:
-            print(f'{person} -> {self.persons[person][0]} -> {self.persons[person][1]}')
-
+    def sendToServer(self, identification):
+        for key, data in self.persons.items():
+            name = str(key)
+            lastMsg = str(data[0])
+            label = ["2fikID", "name","lastMsg"]
+            value = [identification, name, lastMsg]
+            self.server.AddToBuffer(label, value)
+        self.server.SendMessage()
     # def SentMessages(self, identification):
     #     # Fetch all the latest messages sent by the profile 2Fik is currently using
     #     sender_ref = self.db.collection('messages').where(u'from', u'==', identification).order_by(
@@ -41,6 +64,6 @@ class Inbox:
     #         print(f'{person} -> {self.persons[person][0]} -> {self.persons[person][1]}')
 
     # add passed user and their latest messages
-    def addObject(self, user, lastMsg, timestamp):
+    def addObject(self, arr, user, lastMsg, timestamp):
         obj = [lastMsg, timestamp]
-        self.persons[user] = obj
+        arr[user] = obj
