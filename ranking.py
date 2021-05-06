75ee5b73a19c457ebe4f37a4c1df65a82ea65cbc
Tangobaron <raphaeltangobaron@gmail.com>
2021-05-05 22:56:24 -0400

    Completed the Ranking file. Added UIDs to the list of dictionaries. Ordered the lists to be sorted by the number of votes in either categories. Used tabulate to make it look like less of a data nightmare, cheers!

--- ranking.py
+++ ranking.py
@@ -1,4 +1,4 @@
-from firebase_admin import firestore
+import tabulate
 
 from td_client import TDClient
 
@@ -14,20 +14,27 @@ class Ranking:
         # Reference to all of 2Fik profiles
         rank_ref = self.db.collection('profiles').where(u'state', u'==', '2fik').stream()
 
-        # Extracts all the
-        # TODO add logic to make sure people with the same amount of votes are organized depending on the amount of opposite vote and alphabetical order
-
+        # Extract all the names, hotCount and notCount of every 2Fik profiles
         for rank in rank_ref:
             name = rank.get('name')
+            uid = rank.id
             hot_count = rank.get('hotCount')
             not_count = rank.get('notCount')
 
             if hot_count > not_count:
-                hot_dict = {name, hot_count}
+                hot_dict = {'uid': uid, 'name': name, 'hot_count': hot_count}
                 self.hot_leaderboard.append(hot_dict)
+                self.hot_leaderboard.sort(key=lambda i: i['hot_count'], reverse=True)
             elif not_count > hot_count:
-                not_dict = {name, not_count}
+                not_dict = {'uid': uid, 'name': name, 'not_count': not_count}
                 self.not_leaderboard.append(not_dict)
+                self.not_leaderboard.sort(key=lambda i: i['not_count'], reverse=True)
 
-        print(f'hot leaderboard -> {self.hot_leaderboard}')
-        print(f'not leaderboard -> {self.not_leaderboard}')
+        header_hot = self.hot_leaderboard[0].keys()
+        rows_hot = [x.values() for x in self.hot_leaderboard]
+        print(tabulate.tabulate(rows_hot, header_hot))
+        print('------------------------------------------')
+
+        header_not = self.not_leaderboard[0].keys()
+        rows_not = [x.values() for x in self.not_leaderboard]
+        print(tabulate.tabulate(rows_not, header_not))
