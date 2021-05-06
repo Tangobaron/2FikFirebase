4fe454e0b2c199daa5e88491f3f80b5d818417a1
Nicolas Pare <nicolas@laboiteinteractive.com>
2021-05-06 18:20:11 -0400

    remove watchpoint we dont need it anymore

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import time
 
 import firebase_admin
 from firebase_admin import credentials, firestore
-from watchpoints import watch
 from inbox import Inbox as FillInbox
 from ranking import Ranking as FillRanking
 
